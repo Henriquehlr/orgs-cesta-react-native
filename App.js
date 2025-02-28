ac3b7eb24c2087b85782ded66937001a88ce129b
Henriquehlr <henrique-22@hotmail.com>
2025-02-28 00:10:49 -0300

    Refector: Refatoração Codigo

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar, SafeAreaView, View } from "react-native";
-import { Cesta } from "./src/telas/Cesta";
 import {
   useFonts,
   Montserrat_400Regular,
   Montserrat_700Bold,
 } from "@expo-google-fonts/montserrat";
+
+import Cesta from "./src/telas/Cesta";
+import mocks from "./src/mocks/cesta";
 
 export default function App() {
   const [fontCarregada] = useFonts({
@@ -13,13 +15,13 @@ export default function App() {
   });
 
   if (!fontCarregada) {
-    return <View />;
+    return <View />
   }
 
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mocks} />
     </SafeAreaView>
   );
 }
---
+++ src/mocks/cesta.js
@@ -0,0 +1,16 @@
+import logo from "../../assets/logo.png";
+
+const cesta = {
+    topo: {
+        titulo: "Cesta de verduras",
+    }, 
+    detalhes:{
+        nome: "Cesta de verduras",
+        logoFazenda: logo,
+        nomeFazenda: "Henrry Farm",
+        descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto parasua cozinha",
+        preco: "R$ 40,00",
+    }
+}
+
+export default cesta;
